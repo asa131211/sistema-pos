ea1804b78a9af9ab4d4dc83a3b016832693543ca
asa131211 <japvel10@gmail.com>
2025-08-07 00:27:15 -0500

    fix: arreglar rutas alias

--- cash-register.tsx
+++ cash-register.tsx
@@ -212,7 +212,7 @@ export default function CashRegister({ onStatusChange }: CashRegisterProps) {
         </CardHeader>
       </Card>
 
-      {/* Dialog para cerrar caja - Sin resumen */}
+      {/* Dialog para cerrar caja */}
       <Dialog open={showCloseDialog} onOpenChange={setShowCloseDialog}>
         <DialogContent className="max-w-md">
           <DialogHeader>
